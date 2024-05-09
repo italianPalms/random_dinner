29ab653dcd4c93f4b5c875aaa4d0f5f1dae444f1
Eirik Nordsveen <eiriknordsveen@gmail.com>
2024-05-09 18:42:45 +0200

    remove vite and react logo, and add header

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 
@@ -29,21 +27,13 @@ const Dinner = () => {
     }
   }
 
-  const handleSelectChange = (e) => {
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setDinner({...dinner, timeCategory: e.target.value});
   };
   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Welcome to random recipe</h1>
+      <h1>Random dinner selector</h1>
       <div className="card">
         <input
           id='dinnerName'
