ed6033e42589cc1744f2e653fa2db34f3aad4ff2
Eirik Nordsveen <eiriknordsveen@gmail.com>
2024-06-09 21:50:26 +0200

    move the index.html file inside the frontend folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: 'Frontend',
+  publicDir: 'Frontend/public',
   build: {
-    outDir: "dist", 
-  }
-})
+    outDir: '/dist', 
+  },
+  plugins: [react()],
+});
